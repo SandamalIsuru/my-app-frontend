88ec2ddab57a7f698d7494b8d96f923c4d80519a
Isuru Sandamal <isuru@meridian-labs.io>
2023-07-12 20:16:23 +0530

    tailwind configuration and adding necessary folders

---
+++ src/themes/light.js
@@ -0,0 +1,8 @@
+const light = {
+  '--theme-primary': '#0D0F0F',
+  '--theme-secondary': '#1E1E1E',
+  '--theme-text-primary': '#030303',
+  '--theme-text-secondary': '#505052',
+};
+
+export default light;
