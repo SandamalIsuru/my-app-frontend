1bd4abe692a914eaf67e3a4172f40a7e0ce21c17
Isuru Sandamal <isuru@meridian-labs.io>
2023-07-13 13:31:22 +0530

    UI development for signup view

--- src/pages/auth/Signup.js
+++ src/pages/auth/Signup.js
@@ -1,7 +1,212 @@
-import React from 'react';
+import { Checkbox, Divider } from "@mui/material";
+import classNames from "classnames";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+import PageWrapper from "../../components/common/PageWrapper";
+import PasswordTextInput from "../../components/common/PasswordTextInput";
+import SubmitButton from "../../components/common/SubmitButton";
+import TextInput from "../../components/common/TextInput";
+import { getPasswordConstraintSchema } from '../../utils/AuthUtils';
+
+YupPassword(Yup);
+
+const signupSchema = Yup.object().shape({
+  userId: Yup.string().email("Invalid user id").required("User id is required"),
+  ...getPasswordConstraintSchema(),
+});
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isInvalidCredentials, setIsInvalidCredentials] = useState(false);
+  const [keepLoggedIn, setKeepLoggedIn] = useState(false);
 
-  return <div>Sign Up</div>;
+  const isSignInDisabled = (values, errors) => {
+    return (
+      values.userId === "" ||
+      values.password === "" ||
+      values.confirmPassword === "" ||
+      errors.userId ||
+      errors.password ||
+      errors.confirmPassword === ""
+    );
+  };
+
+  const onClickSignIn = () => {
+    navigate("/login");
+  };
+
+  return (
+    <PageWrapper
+      isLoading={isLoading}
+      content={
+        <div
+          className={classNames(
+            "flex flex-col items-center justify-center text-textPrimary w-screen h-full"
+          )}
+        >
+          <div className="w-1/2">
+            <Formik
+              initialValues={{ userId: "", password: "", confirmPassword: "" }}
+              initialErrors={
+                isInvalidCredentials ? { userId: "", password: "", confirmPassword: "" } : {}
+              }
+              validationSchema={signupSchema}
+              validateOnChange={true}
+              validateOnBlur={true}
+              onSubmit={async (values, { resetForm }) => {
+                setIsLoading(true);
+                resetForm();
+              }}
+            >
+              {({ errors, values }) => (
+                <Form className="flex flex-col justify-center items-center">
+                  <div className="text-5xl mb-5 font-light text-center">
+                    <span>Welcome to </span>
+                    <span className="font-bold">myApp</span>
+                  </div>
+                  <Divider className="bg-dividerColor w-1/5 h-1" />
+                  <div className="flex flex-col form-group w-full lg:items-end mt-7">
+                    <div className="lg:w-2/3">
+                      <div className="flex xxs:flex-col lg:flex-row justify-center items-center mb-2">
+                        <div className="xxs:w-full flex lg:justify-center lg:w-1/5">
+                          User ID*
+                        </div>
+                        <div className="xxs:w-full lg:w-4/5">
+                          <Field name="userId" autoComplete="email">
+                            {({ field, form: { touched, errors } }) => (
+                              <TextInput
+                                type="email"
+                                className={"mt-2"}
+                                placeholder="User ID"
+                                field={field}
+                                touched={touched[field.name]}
+                                error={
+                                  (touched[field.name] && errors[field.name]) ||
+                                  isInvalidCredentials
+                                }
+                              />
+                            )}
+                          </Field>
+                          <ErrorMessage
+                            component="div"
+                            name="userId"
+                            className="invalid-feedback text-errorRed text-[12px]"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex xxs:flex-col lg:flex-row items-center mb-2">
+                        <div className="flex xxs:w-full lg:justify-center lg:w-1/5">
+                          Password*
+                        </div>
+                        <div className="xxs:w-full lg:w-4/5">
+                          <Field
+                            name="password"
+                            autoComplete="current-password"
+                          >
+                            {({ field, form: { touched, errors } }) => (
+                              <PasswordTextInput
+                                className={"mt-2"}
+                                placeholder="Password"
+                                field={field}
+                                error={
+                                  (touched[field.name] && errors[field.name]) ||
+                                  isInvalidCredentials
+                                }
+                              />
+                            )}
+                          </Field>
+                          <div className="w-full">
+                            <ErrorMessage
+                              component="div"
+                              name="password"
+                              className=" text-errorRed text-[12px]"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex xxs:flex-col lg:flex-row items-center">
+                        <div className="flex xxs:w-full lg:justify-center lg:w-1/5">
+                          Confirm Password*
+                        </div>
+                        <div className="xxs:w-full lg:w-4/5">
+                          <Field
+                            name="confirmPassword"
+                            autoComplete="current-password"
+                          >
+                            {({ field, form: { touched, errors } }) => (
+                              <PasswordTextInput
+                                className={"mt-2"}
+                                placeholder="Confirm password"
+                                field={field}
+                                error={
+                                  (touched[field.name] && errors[field.name]) ||
+                                  isInvalidCredentials
+                                }
+                              />
+                            )}
+                          </Field>
+                          <div className="w-full">
+                            <ErrorMessage
+                              component="div"
+                              name="confirmPassword"
+                              className=" text-errorRed text-[12px]"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex lg:justify-center items-center mt-2">
+                        <div className="flex justify-center lg:w-1/5"></div>
+                        <div className="flex items-center w-4/5">
+                          <Checkbox
+                            checked={keepLoggedIn}
+                            onChange={() => setKeepLoggedIn(!keepLoggedIn)}
+                            style={{ marginLeft: -10 }}
+                          />
+                          <div>Keep me logged in</div>
+                        </div>
+                      </div>
+                      <div className="flex lg:justify-center items-center mt-2">
+                        <div className="flex justify-center lg:w-1/5"></div>
+                        <div className="flex items-center xxs:w-full lg:w-4/5">
+                          <SubmitButton
+                            className={classNames(
+                              "py-3 xxs:w-full lg:w-[165px] mt-[24px]",
+                              `${
+                                !isSignInDisabled(values, errors)
+                                  ? "bg-primary text-white "
+                                  : "bg-disableColor text-textGray"
+                              }`
+                            )}
+                            label={"REGISTER"}
+                            disabled={isSignInDisabled(values, errors)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-center items-center mt-[24px]">
+                    <span className="text-xs font-normal text-textGray">
+                      {"Already have an account? "}
+                      <span
+                        className="underline text-xs font-normal text-whiteSmoke cursor-pointer"
+                        onClick={onClickSignIn}
+                      >
+                        {"Login here."}
+                      </span>
+                    </span>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      }
+    />
+  );
 };
 export default Signup;
